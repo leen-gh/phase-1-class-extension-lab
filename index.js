dc169dca2475b042d5fd35db19536a708511cd89
leen-gh <leen.ghaleb.d@gmail.com>
2023-12-21 17:50:34 +0300

    solved Assigment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+class Polygon{
+    constructor(ary){
+        this.ary = ary
+    }
+    get countSides(){
+        let sides = this.ary.length
+        return sides
+    }
+
+    get perimeter(){
+        let sum = this.ary.reduce((ary,total)=> ary + total, 0 )
+        return sum 
+    }
+
+    
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        if(this.countSides === 3){
+            return this.ary[0] + this.ary[1] > this.ary[2] && this.ary[0] + this.ary[2] > this.ary[1] && this.ary[1] + this.ary[2] > this.ary[0]
+        }
+        return false
+
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        if(this.countSides === 4){
+            return this.ary[0] === this.ary[1] && this.ary[2] === this.ary[3] && this.ary[2] === this.ary[1] 
+        }
+        return false
+
+    }
+
+    get area(){
+        if(this.isValid){
+            return this.ary[0]*this.ary[0]
+        }
+    }
+}
